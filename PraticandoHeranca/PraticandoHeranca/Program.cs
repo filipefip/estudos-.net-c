eedab8cf0ece44672b1eef39a9f7edb6e55aeaca
filipe.bezerra <filipebezerra021@gmail.com>
2025-12-19 15:15:43 -0300

    Iniciando Curso de APIs

--- PraticandoHeranca/PraticandoHeranca/Program.cs
+++ PraticandoHeranca/PraticandoHeranca/Program.cs
@@ -309,16 +309,16 @@ PublicarCurso()
  
  */
 
-using PraticandoHeranca.Exercicio10;
+//using PraticandoHeranca.Exercicio10;
 
-Instrutor instrutor1 = new Instrutor("Carla", "Back-end");
-ICurso curso1 = new CursoProgramacao("C# com POO", instrutor1);
+//Instrutor instrutor1 = new Instrutor("Carla", "Back-end");
+//ICurso curso1 = new CursoProgramacao("C# com POO", instrutor1);
 
-Instrutor instrutor2 = new Instrutor("Felipe", "UI/UX");
-ICurso curso2 = new CursoDesign("Design de Interfaces", instrutor2);
+//Instrutor instrutor2 = new Instrutor("Felipe", "UI/UX");
+//ICurso curso2 = new CursoDesign("Design de Interfaces", instrutor2);
 
-curso1.ValidarConteudo();
-curso1.PublicarCurso();
+//curso1.ValidarConteudo();
+//curso1.PublicarCurso();
 
-curso2.ValidarConteudo();
-curso2.PublicarCurso();
+//curso2.ValidarConteudo();
+//curso2.PublicarCurso();
---
+++ ScreenSoundAPI/ScreenSoundAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using ScreenSoundAPI.Modelos;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesMusica();
+    } catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }   
+}
